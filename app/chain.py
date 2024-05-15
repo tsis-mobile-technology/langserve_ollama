7f1532396a2329e7aaf39e26c800d825221b766b
taejonggo <gainworld@gmail.com>
2024-05-15 19:43:48 +0900

    Update Ollama model to EEVE-Korean-Instruct-10.8B:latest

--- app/chain.py
+++ app/chain.py
@@ -3,7 +3,8 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 
 # LangChain이 지원하는 다른 채팅 모델을 사용합니다. 여기서는 Ollama를 사용합니다.
-llm = ChatOllama(model="EEVE-Korean-10.8B:latest")
+# llm = ChatOllama(model="EEVE-Korean-10.8B:latest")
+llm = ChatOllama(model="EEVE-Korean-Instruct-10.8B:latest")
 
 # 프롬프트 설정
 prompt = ChatPromptTemplate.from_template("{topic} 에 대하여 간략히 설명해 줘.")
